a08660a3d665fc58deef7f322de432eb451f2b9f
yuuge7 <illuminatiadv.666@gmail.com>
2026-08-20 01:13:49 +0300

    feat: add PDF viewer test utility and revert project version to 1.0.2

--- lib/test_api.dart
+++ lib/test_api.dart
@@ -1,9 +1,8 @@
-import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 void main() {
-  var x = SfPdfViewer.network('https://example.com/test.pdf',
-    onTap: (PdfDocumentCastDetails details) {
+  SfPdfViewer.network('https://example.com/test.pdf',
+    onTap: (details) {
       
     },
   );
